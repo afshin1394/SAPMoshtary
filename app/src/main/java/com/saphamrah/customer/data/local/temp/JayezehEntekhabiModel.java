a7e88b74c36d21eec8a078464b79d7df4fd18fcb
AfshinSamiee <AfshinSamiei94@gmail.com>
2022-11-07 14:43:31 +0330

    -add JayezehEntekhabi

---
+++ app/src/main/java/com/saphamrah/customer/data/local/temp/JayezehEntekhabiModel.java
@@ -0,0 +1,203 @@
+package com.saphamrah.customer.data.local.temp;
+
+import androidx.annotation.NonNull;
+
+public class JayezehEntekhabiModel
+{
+    private int ccJayezeh;
+
+    public void setCcJayezeh(int ccJayezeh) {
+        this.ccJayezeh = ccJayezeh;
+    }
+
+    public int getCcJayezeh() {
+        return this.ccJayezeh;
+    }
+
+
+    private int ccJayezehSatr;
+
+    public void setCcJayezehSatr(int ccJayezehSatr) {
+        this.ccJayezehSatr = ccJayezehSatr;
+    }
+
+    public int getCcJayezehSatr() {
+        return this.ccJayezehSatr;
+    }
+
+
+    private int ccJayezehSatrKala;
+
+    public void setCcJayezehSatrKala(int ccJayezehSatrKala) {
+        this.ccJayezehSatrKala = ccJayezehSatrKala;
+    }
+
+    public int getCcJayezehSatrKala() {
+        return this.ccJayezehSatrKala;
+    }
+
+
+    private int ccKala;
+
+    public void setCcKala(int ccKala) {
+        this.ccKala = ccKala;
+    }
+
+    public int getCcKala() {
+        return this.ccKala;
+    }
+
+
+    private int ccKalaCode;
+
+    public void setCcKalaCode(int ccKalaCode) {
+        this.ccKalaCode = ccKalaCode;
+    }
+
+    public int getCcKalaCode() {
+        return this.ccKalaCode;
+    }
+
+
+    private int ccKalaCodeAsli;
+
+    public void setCcKalaCodeAsli(int ccKalaCodeAsli) {
+        this.ccKalaCodeAsli = ccKalaCodeAsli;
+    }
+
+    public int getCcKalaCodeAsli() {
+        return this.ccKalaCodeAsli;
+    }
+
+
+    private String CodeKala;
+
+    public void setCodeKala(String CodeKala) {
+        this.CodeKala = CodeKala;
+    }
+
+    public String getCodeKala() {
+        return this.CodeKala;
+    }
+
+
+    private String CodeKalaOld;
+
+    public void setCodeKalaOld(String CodeKalaOld) {
+        this.CodeKalaOld = CodeKalaOld;
+    }
+
+    public String getCodeKalaOld() {
+        return this.CodeKalaOld;
+    }
+
+
+    private String NameKala;
+
+    public void setNameKala(String NameKala) {
+        this.NameKala = NameKala;
+    }
+
+    public String getNameKala() {
+        return this.NameKala;
+    }
+
+
+    private int ccMarkazForosh;
+
+    public void setCcMarkazForosh(int ccMarkazForosh) {
+        this.ccMarkazForosh = ccMarkazForosh;
+    }
+
+    public int getCcMarkazForosh() {
+        return this.ccMarkazForosh;
+    }
+
+
+    private float MablaghForosh;
+
+    public void setMablaghForosh(float MablaghForosh) {
+        this.MablaghForosh = MablaghForosh;
+    }
+
+    public float getMablaghForosh() {
+        return this.MablaghForosh;
+    }
+
+
+    private int ccNoeMoshtary;
+
+    public void setCcNoeMoshtary(int ccNoeMoshtary) {
+        this.ccNoeMoshtary = ccNoeMoshtary;
+    }
+
+    public int getCcNoeMoshtary() {
+        return this.ccNoeMoshtary;
+    }
+
+
+    private int CodeNoe;
+
+    public void setCodeNoe(int CodeNoe) {
+        this.CodeNoe = CodeNoe;
+    }
+
+    public int getCodeNoe() {
+        return this.CodeNoe;
+    }
+
+
+    private int ccTakhfifHajmi;
+
+    public void setCcTakhfifHajmi(int ccTakhfifHajmi) {
+        this.ccTakhfifHajmi = ccTakhfifHajmi;
+    }
+
+    public int getCcTakhfifHajmi() {
+        return this.ccTakhfifHajmi;
+    }
+
+
+    private int Id;
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getId() {
+        return this.Id;
+    }
+
+
+    private int ExtraProp_Tedad;
+
+    public int getExtraProp_Tedad() {
+        return ExtraProp_Tedad;
+    }
+
+    public void setExtraProp_Tedad(int extraProp_Tedad) {
+        ExtraProp_Tedad = extraProp_Tedad;
+    }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ccJayezeh=" + ccJayezeh +
+                ", ccJayezehSatr=" + ccJayezehSatr +
+                ", ccJayezehSatrKala=" + ccJayezehSatrKala +
+                ", ccKala=" + ccKala +
+                ", ccKalaCode=" + ccKalaCode +
+                ", ccKalaCodeAsli=" + ccKalaCodeAsli +
+                ", CodeKala='" + CodeKala + '\'' +
+                ", CodeKalaOld='" + CodeKalaOld + '\'' +
+                ", NameKala='" + NameKala + '\'' +
+                ", ccMarkazForosh=" + ccMarkazForosh +
+                ", MablaghForosh=" + MablaghForosh +
+                ", ccNoeMoshtary=" + ccNoeMoshtary +
+                ", CodeNoe=" + CodeNoe +
+                ", ccTakhfifHajmi=" + ccTakhfifHajmi +
+                ", Id=" + Id +
+                ", ExtraProp_Tedad=" + ExtraProp_Tedad;
+    }
+}
